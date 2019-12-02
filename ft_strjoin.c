16dfc0dbbb11d9224da1fd7679d70d5f153a899b
bn9319 <bas-nijland@outlook.com>
2019-12-02 18:53:22 +0100

    ..

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,30 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   testmain.c                                         :+:    :+:            */
+/*   ft_strjoin.c                                       :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: bnijland <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/11/21 13:50:58 by bnijland      #+#    #+#                 */
-/*   Updated: 2019/11/30 23:23:53 by bnijland      ########   odam.nl         */
+/*   Created: 2019/11/07 19:14:02 by bnijland      #+#    #+#                 */
+/*   Updated: 2019/11/18 19:21:54 by bnijland      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 
-int	main(void)
+char	*ft_strjoin(char  *s1, char  *s2)
 {
-	int fd;
-	char *line;
-	
-	fd = open("hello.txt", O_RDONLY);
-//	printf("%d\n", fd);
-	while (get_next_line(fd, line) == 1)
+	char	*s3;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (s1 == 0 || s2 == 0)
+		return (0);
+	s3 = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (s3 == 0)
+		return (0);
+	while (s1[i])
 	{
-	  printf("%d\n", get_next_line(fd, line));
-	  	 			}
-//	printf("%d\n", get_next_line(fd, line));
-	return(0);
+		s3[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		s3[i] = s2[j];
+		i++;
+		j++;
+	}
+	s3[i] = 0;
+	return (s3);
 }
--- ft_strlen.c
+++ ft_strlen.c
@@ -1,30 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   testmain.c                                         :+:    :+:            */
+/*   ft_strlen.c                                        :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: bnijland <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/11/21 13:50:58 by bnijland      #+#    #+#                 */
-/*   Updated: 2019/11/30 23:23:53 by bnijland      ########   odam.nl         */
+/*   Created: 2019/10/28 15:04:54 by bnijland      #+#    #+#                 */
+/*   Updated: 2019/11/18 19:05:38 by bnijland      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 
-int	main(void)
+int	ft_strlen(const char *s)
 {
-	int fd;
-	char *line;
-	
-	fd = open("hello.txt", O_RDONLY);
-//	printf("%d\n", fd);
-	while (get_next_line(fd, line) == 1)
+	int i;
+
+	i = 0;
+	while (s[i])
 	{
-	  printf("%d\n", get_next_line(fd, line));
-	  	 			}
-//	printf("%d\n", get_next_line(fd, line));
-	return(0);
+		i++;
+	}
+	return (i);
 }
